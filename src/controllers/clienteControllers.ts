f8f67bae85ee2337b5128164c19eeea834b4175a
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-03-31 18:35:39 -0300

    Update
    Date time Brazil

--- src/controllers/clienteControllers.ts
+++ src/controllers/clienteControllers.ts
@@ -11,7 +11,6 @@ async function enviarMensagem(
   mensagem: string
 ): Promise<void> {
   try {
-    // Remover '+' do início do telefone, se existir
     const numeroFormatado = telefone.replace("+", "");
     console.log(`Tentando enviar mensagem para ${numeroFormatado}`);
     await client.sendMessage(`${numeroFormatado}@c.us`, mensagem);
@@ -21,15 +20,17 @@ async function enviarMensagem(
   }
 }
 
-// Função para agendar mensagens
+// Função para agendar mensagens com ajuste de fuso horário
 function agendarMensagem(
   telefone: string,
   date: string,
   time: string,
   mensagem: string
 ): void {
-  // Construir horário com luxon
-  const horarioAgendado = DateTime.fromISO(`${date}T${time}`);
+  // Construir horário com luxon ajustando para o fuso horário de São Paulo
+  const horarioAgendado = DateTime.fromISO(`${date}T${time}`, {
+    zone: "America/Sao_Paulo",
+  });
 
   // Verificar se o horário é válido
   if (!horarioAgendado.isValid) {
@@ -55,7 +56,6 @@ function agendarMensagem(
 export async function createCliente(req: Request, res: Response) {
   const { name, date, time, service, barber, phone } = req.body;
 
-  // Validação dos campos obrigatórios
   if (!name || !date || !time || !service || !barber || !phone) {
     return res
       .status(422)
@@ -66,12 +66,9 @@ export async function createCliente(req: Request, res: Response) {
     const data = req.body;
     const cliente = await ClienteModel.create(data);
 
-    // Mensagem personalizada para o cliente
     const mensagem = `Olá ${cliente.name}, está quase na hora do seu corte! Serviço: ${cliente.service} com ${cliente.barber} às ${cliente.time}.`;
-    const horarioAgendado = `${cliente.date}T${cliente.time}`;
-
     if (typeof cliente.phone === "string") {
-      agendarMensagem(cliente.phone, horarioAgendado, mensagem);
+      agendarMensagem(cliente.phone, cliente.date, cliente.time, mensagem);
     } else {
       throw new Error("Número de telefone inválido ou ausente.");
     }
@@ -83,81 +80,6 @@ export async function createCliente(req: Request, res: Response) {
       return res.status(500).json({ error: "Por favor, tente mais tarde!" });
     } else {
       Logger.error("Erro desconhecido!");
-      return res.status(500).json({ error: "Por favor, tente mais tarde!" });
-    }
-  }
-}
-
-// Buscar cliente por ID
-export async function findClienteById(req: Request, res: Response) {
-  try {
-    const id = req.params.id;
-    const cliente = await ClienteModel.findById(id);
-
-    if (!cliente) {
-      return res.status(404).json({ error: "O Cliente não existe!" });
-    }
-
-    return res.status(200).json(cliente);
-  } catch (e: unknown) {
-    if (e instanceof Error) {
-      Logger.error(`Erro no sistema: ${e.message}`);
-      return res.status(500).json({ error: "Por favor, tente mais tarde!" });
-    }
-  }
-}
-
-// Listar todos os clientes
-export async function getAllClientes(req: Request, res: Response) {
-  try {
-    const clientes = await ClienteModel.find();
-    return res.status(200).json(clientes);
-  } catch (e: unknown) {
-    if (e instanceof Error) {
-      Logger.error(`Erro no sistema: ${e.message}`);
-      return res.status(500).json({ error: "Por favor, tente mais tarde!" });
-    }
-  }
-}
-
-// Remover cliente
-export async function RemoveCliente(req: Request, res: Response) {
-  try {
-    const id = req.params.id;
-    const cliente = await ClienteModel.findById(id);
-
-    if (!cliente) {
-      return res.status(404).json({ error: "O Cliente não existe!" });
-    }
-
-    await cliente.deleteOne();
-
-    return res.status(200).json({ message: "Cliente removido com sucesso!" });
-  } catch (e: unknown) {
-    if (e instanceof Error) {
-      Logger.error(`Erro no sistema: ${e.message}`);
-      return res.status(500).json({ error: "Por favor, tente mais tarde!" });
-    }
-  }
-}
-
-// Atualizar cliente
-export async function updateCliente(req: Request, res: Response) {
-  try {
-    const id = req.params.id;
-    const data = req.body;
-    const cliente = await ClienteModel.findById(id);
-
-    if (!cliente) {
-      return res.status(404).json({ error: "O Cliente não existe!" });
-    }
-
-    await ClienteModel.updateOne({ _id: id }, data);
-
-    return res.status(200).json({ cliente });
-  } catch (e: unknown) {
-    if (e instanceof Error) {
-      Logger.error(`Erro no sistema: ${e.message}`);
       return res.status(500).json({ error: "Por favor, tente mais tarde!" });
     }
   }
