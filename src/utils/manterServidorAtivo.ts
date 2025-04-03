4a7a28a4ddcb155c4cfff7f0d5abf293c14fbd55
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-04-02 21:20:35 -0300

    Add return

--- src/utils/manterServidorAtivo.ts
+++ src/utils/manterServidorAtivo.ts
@@ -2,10 +2,11 @@ export default function manterServidorAtivo() {
   setInterval(() => {
     const agora = new Date();
     const hora = agora.getHours();
-    if (hora >= 8 && hora < 22) {
+    if (hora >= 8 && hora < 23) {
       console.log("Servidor está ativo durante o período de funcionamento.");
     } else {
       console.log("Servidor fora do período de funcionamento.");
+      return;
     }
   }, 600000); // Executa a cada 10 minutos
 }
