94210d160b4568d6ba283886b2783e549c42a558
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-04-03 20:23:49 -0300

    Update req active API

--- src/utils/manterServidorAtivo.js
+++ src/utils/manterServidorAtivo.js
@@ -1,17 +1,42 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = manterServidorAtivo;
+const axios_1 = __importDefault(require("axios"));
 let intervalo;
 function manterServidorAtivo() {
-    intervalo = setInterval(() => {
+    intervalo = setInterval(() => __awaiter(this, void 0, void 0, function* () {
         const agora = new Date();
         const hora = agora.getHours();
-        if (hora >= 8 && hora < 23) {
-            console.log("Servidor está ativo durante o período de funcionamento.");
+        if (hora >= 8 && hora < 22) {
+            try {
+                // Faz a requisição GET para a própria aplicação
+                const response = yield axios_1.default.get("https://backendbarbearia-6205.onrender.com/api/test"); // Ajuste a URL conforme necessário
+                console.log("Requisição bem-sucedida:", response.data);
+            }
+            catch (error) {
+                if (error instanceof Error) {
+                    console.error("Erro:", error.message);
+                }
+                else {
+                    console.error("Erro desconhecido:", error);
+                }
+            }
         }
         else {
-            console.log("Servidor fora do período de funcionamento.");
-            clearInterval(intervalo); // Cancela o intervalo
+            console.log("Fora do período de funcionamento. Cancelando intervalo.");
+            clearInterval(intervalo);
         }
-    }, 600000); // Executa a cada 10 minutos
+    }), 600000); // Executa a cada 10 minutos (600.000ms)
 }
--- src/utils/manterServidorAtivo.ts
+++ src/utils/manterServidorAtivo.ts
@@ -1,14 +1,29 @@
+import axios from "axios";
+
 let intervalo: NodeJS.Timeout;
 
 export default function manterServidorAtivo() {
-  intervalo = setInterval(() => {
+  intervalo = setInterval(async () => {
     const agora = new Date();
     const hora = agora.getHours();
-    if (hora >= 8 && hora < 23) {
-      console.log("Servidor está ativo durante o período de funcionamento.");
+
+    if (hora >= 8 && hora < 22) {
+      try {
+        // Faz a requisição GET para a própria aplicação
+        const response = await axios.get(
+          "https://backendbarbearia-6205.onrender.com/api/test"
+        ); // Ajuste a URL conforme necessário
+        console.log("Requisição bem-sucedida:", response.data);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error("Erro:", error.message);
+        } else {
+          console.error("Erro desconhecido:", error);
+        }
+      }
     } else {
-      console.log("Servidor fora do período de funcionamento.");
-      clearInterval(intervalo); // Cancela o intervalo
+      console.log("Fora do período de funcionamento. Cancelando intervalo.");
+      clearInterval(intervalo);
     }
-  }, 600000); // Executa a cada 10 minutos
+  }, 600000); // Executa a cada 10 minutos (600.000ms)
 }
